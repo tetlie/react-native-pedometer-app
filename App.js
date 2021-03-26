c7b8de7e1588ce61da7507871ee124da90074c5c
tetlie <marius.tetlie@gmail.com>
2021-03-26 10:03:52 +0100

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,156 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import {
+  Alert,
+  Button,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { Pedometer } from "expo-sensors";
 
 export default function App() {
+  const [pastStepCount, setPastStepCount] = useState(8340);
+  const [currentStepCount, setCurrentStepCount] = useState(0);
+  const [isPedometerAvailable, setIsPedometerAvailable] = useState("Checking");
+
+  const [stepGoal, setStepGoal] = useState(10000);
+  const [goalPercent, setGoalPercent] = useState(0);
+  const [goalReached, setGoalReached] = useState(false);
+
+  const [userName, setUserName] = useState("User");
+
+  useEffect(() => {
+    Pedometer.isAvailableAsync().then(
+      (result) => {
+        console.log("isAvailableAsyncResult:", result);
+        setIsPedometerAvailable(String(result));
+      },
+      (error) => {
+        console.log("isAvailableAsyncError:", error);
+      }
+    );
+
+    const startDate = new Date();
+    const endDate = new Date();
+    startDate.setDate(endDate.getDate() - 1);
+
+    Pedometer.getStepCountAsync(startDate, endDate).then(
+      (result) => {
+        console.log("getStepCountAsyncResult:", result);
+        setPastStepCount(result.steps);
+      },
+      (error) => {
+        console.log("getStepCountAsyncError:", error);
+      }
+    );
+
+    // Sett lytter lytter
+    const subscription = Pedometer.watchStepCount((result) => {
+      console.log("Watch:", result);
+      setCurrentStepCount(result.steps);
+    });
+
+    // Avslutt lytter
+    return () => {
+      subscription.remove();
+    };
+  }, []);
+
+  useEffect(() => {
+    setGoalPercent((pastStepCount / stepGoal) * 100);
+    pastStepCount >= stepGoal && setGoalReached(true);
+  }, [pastStepCount, stepGoal]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <TouchableOpacity
+        onPress={() =>
+          Alert.prompt("Username", "Set a new username", (input) =>
+            setUserName(input)
+          )
+        }
+      >
+        <Text style={styles.username}>{userName}</Text>
+      </TouchableOpacity>
+
+      <Text style={styles.metadata}>
+        {new Date(Date.now()).toLocaleDateString()}
+      </Text>
+      <Text style={styles.metadata}>Step count today</Text>
+      <View
+        style={[
+          styles.dataContainer,
+          { backgroundColor: goalReached ? "lime" : "#e0e0de" },
+        ]}
+      >
+        <Text style={styles.count}>
+          {pastStepCount} / {stepGoal}
+        </Text>
+      </View>
+      <View
+        style={{
+          height: 20,
+          width: "80%",
+          backgroundColor: "#e0e0de",
+          borderRadius: 50,
+          margin: 50,
+        }}
+      >
+        <View
+          style={{
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100%",
+            width: goalPercent >= 100 ? "100%" : `${goalPercent}%`,
+            backgroundColor: goalPercent >= 100 ? "lime" : "darkgrey",
+            position: "absolute",
+            borderRadius: 50,
+            textAlign: "right",
+          }}
+        >
+          <Text>{Math.floor(goalPercent)}%</Text>
+        </View>
+      </View>
+      <Button
+        title="Set new goal"
+        onPress={() =>
+          Alert.prompt("New goal", "Set a new daily step goal", (input) =>
+            setStepGoal(Number(input))
+          )
+        }
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    width: "100%",
+    padding: 20,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  count: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  dataContainer: {
+    padding: 20,
+    width: "80%",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 20,
+  },
+  metadata: {
+    margin: 20,
+  },
+  username: {
+    fontSize: 20,
+    fontWeight: "bold",
   },
 });
---
+++ README.md
@@ -0,0 +1,37 @@
+# React Native App
+
+This is a React Native project.
+
+## Installation
+
+Sudo install the [expo client](https://pip.pypa.io/en/stable/).
+
+```bash
+sudo npm install -g expo-cli
+```
+
+## Dependancies
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install foobar.
+
+```bash
+expo install expo-sensors
+```
+
+## Running
+
+```bash
+npm start
+```
+
+Open [http://localhost:19002](http://localhost:19002) with your browser to see the result.
+
+## Usage
+
+```python
+import foobar
+
+foobar.pluralize('word') # returns 'words'
+foobar.pluralize('goose') # returns 'geese'
+foobar.singularize('phenomena') # returns 'phenomenon'
+```
